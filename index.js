95e356325accd19d07061d37b8ba4c28429ecd23
Temu4 <artem.vorobjov@gmail.com>
2022-08-19 23:59:30 +0200

    add change year functionality

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-import moment from './node_modules/moment/dist/moment.js';
+import calcRemainingTime from './calcRemainingTime.js';
 
 const wrapper = document.getElementById('root');
 
-const currentYear = moment().year();
-const nextNewYear = moment(`${currentYear + 1}`);
+let targetYear = new Date().getFullYear() + 1;
 
-const calcRemainingTime = (targetDate) => {
-  const todayDate = moment();
-
-  const diffInDays = targetDate.diff(todayDate, 'd');
-
-  // moment.add() mutates initial date
-  todayDate.add(diffInDays, 'd');
-
-  const diffInHours = targetDate.diff(todayDate, 'h');
-
-  todayDate.add(diffInHours, 'h');
-
-  const diffInMinutes = targetDate.diff(todayDate, 'm');
-
-  todayDate.add(diffInMinutes, 'm');
-
-  const diffInSeconds = targetDate.diff(todayDate, 's');
-
-  return {diffInDays, diffInHours, diffInMinutes, diffInSeconds};
+const renderInitialMarkup = () => {
+  wrapper.innerHTML = `<p>Time left to <b id="targetYear"></b> New Year:</p>
+                      <button id="increaseBtn">+</button>
+                      <button id="decreaseBtn">-</button>
+                      <h1 id="countdown"></h1>`;
 };
 
-setInterval(() => {
-  const {diffInDays, diffInHours, diffInMinutes, diffInSeconds} = calcRemainingTime(nextNewYear);
+document.addEventListener('DOMContentLoaded', () => {
+  renderInitialMarkup();
 
-  wrapper.innerHTML = `<p>Time left to New Year:</p><h1>${diffInDays} days, ${diffInHours} hours, ${diffInMinutes} minutes, ${diffInSeconds} sec</h1>`;
-}, 1000);
+  const yearWrapper = document.getElementById('targetYear');
+  const countdownWrapper = document.getElementById('countdown');
+  const increaseBtn = document.getElementById('increaseBtn');
+  const decreaseBtn = document.getElementById('decreaseBtn');
+
+  yearWrapper.innerText = targetYear;
+
+  const changeYear = (value) => {
+    targetYear += value;
+    yearWrapper.innerText = targetYear;
+  };
+
+  increaseBtn.addEventListener('click', () => changeYear(1));
+  decreaseBtn.addEventListener('click', () => changeYear(-1));
+
+  setInterval(() => {
+    const {diffInYears, diffInDays, diffInHours, diffInMinutes, diffInSeconds} =
+      calcRemainingTime(targetYear);
+
+    countdownWrapper.innerText = `${diffInYears} years, ${diffInDays} days, ${diffInHours} hours, ${diffInMinutes} minutes, ${diffInSeconds} sec`;
+  }, 1000);
+});
